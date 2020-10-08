10238e631ca70bd0e636b94ee9625d930fcc0803
Heo Jong Tae <heo9910@naver.com>
2020-10-09 01:46:37 +0900

    .

--- README.md
+++ README.md
@@ -8,7 +8,4 @@
   - 스프링 프로젝트 기본 구조
 2. spring-study-02-core
   - 애너테이션 사용하기 
-    - @Configuration, @Bean, @Component
-    - @Qualifier, @Autowire
-    - @Primary, @Resourece, @Scope
-  
+    - @Configuration, @Bean, @Component, @Qualifier, @Autowire, @Primary, @Resourece, @Scope, @PropertySorce, @PostConstruct
--- spring-study-02-core/src/main/java/com/apress/springrecipes/shop/config/ShopConfiguration.java
+++ spring-study-02-core/src/main/java/com/apress/springrecipes/shop/config/ShopConfiguration.java
@@ -1,16 +1,27 @@
 package com.apress.springrecipes.shop.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import com.apress.springrecipes.shop.Battery;
 import com.apress.springrecipes.shop.Disc;
 import com.apress.springrecipes.shop.Product;
 
+@PropertySource("classpath:discounts.properties")
 @ComponentScan("com.apress.springrecipes.shop")
 @Configuration
 public class ShopConfiguration {
+    
+    /**
+     * properties 파일에서 endofyear.discount의 값을 읽어서 할당
+     * endofyear.discount가 없다면 디폴드 값이 0으로 설정됨
+     */
+    @Value("${endofyear.discount:0}")
+    private double specialEndofyearDiscountField;
     
     @Bean
     public Product aaa() {
@@ -31,5 +42,16 @@ public class ShopConfiguration {
         Disc p3 = new Disc("DVD-RW", 3.0);
         p3.setCapacity(700);
         return p3;
+    }
+    
+    
+    /**
+     * 
+     * @PropertySource 사용시 세트로 함께 사용한다.
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer 
+        propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
