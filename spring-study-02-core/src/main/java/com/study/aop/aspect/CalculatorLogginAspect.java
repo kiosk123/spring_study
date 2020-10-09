b55b275bc89ad7a451c4066fe527c00b0ea207d8
Heo Jong Tae <heo9910@naver.com>
2020-10-09 18:45:22 +0900

    .

--- spring-study-02-core/src/main/java/com/study/aop/aspect/CalculatorLogginAspect.java
+++ spring-study-02-core/src/main/java/com/study/aop/aspect/CalculatorLogginAspect.java
@@ -1,6 +1,13 @@
 package com.study.aop.aspect;
 
+import java.util.Arrays;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -19,15 +26,72 @@ public class CalculatorLogginAspect {
     
     /**
      * 접근 제한자 public protected private 모두 매치하면서
-     * 모든 반환형을 매치하고 ArithmeticCalculator의 n개(0개포함)인수를 갖고 있는 add() 메소드 호출전
-     * 실행
+     * 모든 반환형을 매치하고 ArithmeticCalculator의 
+     * n개(0개포함)의 인수를 갖고 있는 add() 메소드 호출전 실행
      */
     @Before("execution(* com.study.aop.ArithmeticCalculator.add(..))")
     public void logBefore(JoinPoint jp) {
         log.info("The method add() begins");
         Object[] params = jp.getArgs();
         if (params != null && params.length > 0) {
-            
+           int num = 1;
+           for (Object param : params) {
+               log.info("passing the param {} : value {}", num, param);
+               num++;
+           }
         }
+    }
+    
+    /**
+     * @After에 선언된 포인트 컷의 조인포인트 끝난 후 실행
+     * 조인포인트 실행 후 예외가 발생하든, 성공하든지 무조건 실행
+     */
+    @After("execution(* *.*(..))")
+    public void logAfter(JoinPoint jp) {
+        log.info("The method {} end",jp.getSignature().getName());
+    }
+    
+    /**
+     * @AfterReturning 조인포인트의 실행 성공여부와 상관없이 동작
+     * 조인포인트에서 값을 반환하는 경우에만 실행하고 싶을때 사용
+     */
+    @AfterReturning(value = "execution(* *.*(..))", returning = "result")
+    public void logAfterReturning(JoinPoint jp, Object result) {
+        log.info("The method {}() ends with {}", jp.getSignature().getName(), result);
+    }
+    
+    /**
+     * @AfterThrowing
+     * 조인 포인트에서 에러가 발생했을 때 사용
+     */
+    @AfterThrowing(value = "execution(* *.*(..))", throwing = "e")
+    public void logAfterThrowing(JoinPoint jp, Throwable e) {
+        log.error("An exception {} has been thrown in {}()", 
+                e, 
+                jp.getSignature().getName());
+    }
+    
+    /**
+     *  @Around 가장 강력한 기능의 어드바이스
+     *  조인포인트를 완전 장악한다.(조인포인트를 감쌈)
+     *  조인포인트를 언제 실행할지, 실행을 하지말지, 계속 실행할지 여부도 제어가능하다.
+     */
+    @Around("execution(* *.*(..))")
+    public Object logAround(ProceedingJoinPoint jp) throws Throwable {
+        
+        log.info("The method {}() begins with {}", jp.getSignature().getName(),
+                Arrays.toString(jp.getArgs()));
+        Object result = null;
+        try {
+            result = jp.proceed();
+            log.info("The method {}() ends with ", jp.getSignature().getName());
+        }
+        catch (IllegalArgumentException e) {
+            log.error("Illegal argument {} in {}()", Arrays.toString(jp.getArgs()),
+                    jp.getSignature().getName());
+            throw e;
+        }
+
+        return result;
     }
 }
--- spring-study-02-core/src/main/java/com/study/aop/main/AOPMain.java
+++ spring-study-02-core/src/main/java/com/study/aop/main/AOPMain.java
@@ -4,6 +4,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.study.aop.ArithmeticCalculator;
+import com.study.aop.UnitCalculator;
+import com.study.aop.UnitCalculatorImpl;
 import com.study.aop.config.CalculatorConfiguration;
 
 public class AOPMain {
@@ -14,6 +16,13 @@ public class AOPMain {
         ArithmeticCalculator arithmeticCalculator
             = context.getBean("arithmeticCalculator", ArithmeticCalculator.class);
         arithmeticCalculator.add(1, 2);
+        
+        UnitCalculator unitCalculator 
+            = context.getBean("unitCalculator", UnitCalculator.class);
+        unitCalculator.kilogramToPound(10);
+        
+        arithmeticCalculator.sub(12, 0);
+        arithmeticCalculator.div(12, 0);
 
     }
 
