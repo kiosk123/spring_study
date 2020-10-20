0df6ac8b9afac417c05b59f094ba75bf06b26364
Heo Jong Tae <heo9910@naver.com>
2020-10-21 00:16:15 +0900

    .

---
+++ spring-study-03-mvc-11-excel-pdf-view/src/main/java/com/study/mvc/config/InterceptorConfiguration.java
@@ -0,0 +1,32 @@
+package com.study.mvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.study.mvc.web.ExtensionInterceptor;
+import com.study.mvc.web.MeasurementInterceptor;
+
+@Configuration
+@ComponentScan("com.apress.springrecipes.court")
+public class InterceptorConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(measurementInterceptor());
+        registry.addInterceptor(summaryReportInterceptor()).addPathPatterns("/reservationSummary*");
+
+    }
+
+    @Bean
+    public MeasurementInterceptor measurementInterceptor() {
+        return new MeasurementInterceptor();
+    }
+
+    @Bean
+    public ExtensionInterceptor summaryReportInterceptor() {
+        return new ExtensionInterceptor();
+    }
+}
---
+++ spring-study-03-mvc-11-excel-pdf-view/src/main/java/com/study/mvc/domain/SportTypeConverter.java
@@ -0,0 +1,23 @@
+package com.study.mvc.domain;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import com.study.mvc.service.ReservationService;
+
+@Component
+public class SportTypeConverter implements Converter<String, SportType> {
+
+    private final ReservationService reservationService;
+
+    public SportTypeConverter(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @Override
+    public SportType convert(String source) {
+        int sportTypeId = Integer.parseInt(source);
+        SportType sportType = reservationService.getSportType(sportTypeId);
+        return sportType;
+    }
+}
